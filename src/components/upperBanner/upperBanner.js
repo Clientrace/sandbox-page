115f4f6788c5afb43aab1f344505ed6f48aca0f8
Clarence Penaflor <kimc.penaflor@gmail.com>
2022-09-21 05:24:04 +0800

    add upper header

--- src/components/upperBanner/upperBanner.js
+++ src/components/upperBanner/upperBanner.js
@@ -1,13 +1,16 @@
 
-import styles from "./upperBanner.module.css"
+import styles from "./upperBanner.module.scss"
 import { motion } from "framer-motion";
 
 const UpperBanner = () => {
   return (
     <motion.div className={styles.main}>
       <motion.div className={styles.comp1}>
-        Hi, Babi
+        Hi Babi
         <img src="/imgs/penguin.gif"/>
+      </motion.div>
+      <motion.div className={styles.comp2}>
+        <i>(Or Babe, Leo, Leys, Beb, Leonora)</i>
       </motion.div>
     </motion.div>
   )
