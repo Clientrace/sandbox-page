74e1064f112dba2211048f0c1cd75e19e77928b5
Clarence Penaflor <kimc.penaflor@gmail.com>
2022-09-21 08:49:20 +0800

    Initial dist subtree commit

---
+++ _next/static/gIsBUMzeidZvBULO5Uyjs/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/600-42453ff3a9f51a69.js","static/css/56775131a2cbc81f.css","static/chunks/pages/index-f1fba3b1cbc98e3b.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
